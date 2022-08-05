0412b6df1314686d2fc44667a147c0afabddead7
Alan Shaw <alan.shaw@protocol.ai>
2022-08-05 10:18:03 +0100

    feat: fast sharded dir listing

--- src/resolvers/unixfs-v1/content/hamt-sharded-directory.js
+++ src/resolvers/unixfs-v1/content/hamt-sharded-directory.js
@@ -37,22 +37,32 @@ const hamtShardedDirectoryContent = (cid, node, unixfs, path, resolve, depth, bl
 async function * listDirectory (node, path, resolve, depth, blockstore, options) {
   const links = node.Links
 
-  for (const link of links) {
-    const name = link.Name != null ? link.Name.substring(2) : null
-
-    if (name) {
-      const result = await resolve(link.Hash, name, `${path}/${name}`, [], depth + 1, blockstore, options)
-
-      yield result.entry
-    } else {
-      // descend into subshard
-      const block = await blockstore.get(link.Hash)
-      node = decode(block)
-
-      for await (const file of listDirectory(node, path, resolve, depth, blockstore, options)) {
-        yield file
+  const results = (async function * () {
+    const resultPromises = links.map(async l => {
+      try {
+        const name = l.Name != null ? l.Name.substring(2) : null
+        if (name) {
+          const result = await resolve(l.Hash, name, `${path}/${name}`, [], depth + 1, blockstore, options)
+          return { entries: [result.entry] }
+        } else {
+          // descend into subshard
+          const block = await blockstore.get(l.Hash)
+          node = decode(block)
+          return { entries: listDirectory(node, path, resolve, depth, blockstore, options) }
+        }
+      } catch (error) {
+        return { error }
       }
+    })
+    for (const promise of resultPromises) {
+      const res = await promise
+      if ('error' in res) throw res.error
+      yield res.entries
     }
+  })()
+
+  for await (const entries of results) {
+    yield * entries
   }
 }
 
